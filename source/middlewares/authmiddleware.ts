df41ee27a5e3bf08c4a3ff0f74e7eabba6d9da8a
ManuelTello <93638568+ManuelTello@users.noreply.github.com>
2024-06-20 00:55:18 -0300

    Loging functions removed

--- source/middlewares/authmiddleware.ts
+++ source/middlewares/authmiddleware.ts
@@ -4,7 +4,6 @@ import "dotenv/config";
 
 export function authMiddleware(req: Request, res: Response, next: NextFunction): void {
     const auth_header: string | undefined = req.headers.authorization;
-    const a: string = `ApiKey ${process.env.API_KEY}`;
     if (auth_header && auth_header == `ApiKey ${process.env.API_KEY}`) {
         next();
     } else {
